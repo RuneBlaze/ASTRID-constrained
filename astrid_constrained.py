45f38a4c1fd8cc11563569c76cd5ff5a62fa9fe7
runeblaze <runeblaze@excite.co.jp>
2021-08-05 01:59:33 -0400

    now with NJst options

--- astrid_constrained.py
+++ astrid_constrained.py
@@ -5,20 +5,34 @@ import nj
 import treeswift as ts
 import argparse
 
+def starlize(tree):
+    tocontract = []
+    for n in tree.traverse_preorder(leaves=False,internal=True):
+        tocontract.append(n)
+    for n in tocontract:
+        n.contract()
+    return tree
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='run ASTRID')
     parser.add_argument("-i", "--input", type=str,
                         help="Input tree list file", required=True)
     parser.add_argument("-o", "--output", type=str,
                         help="Output tree file", required=True)
-    parser.add_argument("-t", "--tree", type=str, help="constraint trees", required=True)
-
+    parser.add_argument("-t", "--tree", type=str, help="constraint trees")
     args = parser.parse_args()
-
-    with open(args.tree) as fh:
-        tree = ts.read_tree_newick(fh.read())
+    tree = None
+    if args.tree:
+        with open(args.tree) as fh:
+            tree = ts.read_tree_newick(fh.read())
+    else:
+        with open(args.input, "r") as fh:
+            for l in fh:
+                tree = ts.read_tree_newick(l)
+                starlize(tree)
+                break
     
-    genes = open(args.input, "r").readlines()
+    with open(args.input, "r") as o: genes = o.readlines()
     ts = ad.get_ts(genes)
     D = ad.mk_distance_matrix(ts, genes)
     merged_tree = nj.treeresolve(tree, ts, D)
